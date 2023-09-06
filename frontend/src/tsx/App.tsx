94b95036eec238cd1bf7dcbc02ae21e49c44b7ee
Andres0663 <anma663@gmail.com>
2023-09-05 22:38:50 -0600

    Limpiar App.tsx

---
+++ frontend/src/tsx/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../styles/App.scss'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>PJ TimeLine</h1>
+      
+    </>
+  )
+}
+
+export default App
